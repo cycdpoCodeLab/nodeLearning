0fc12fd4b2a993171473a37c3dc6be5864f2f9e7
cycjimmy <cycjimmy@gmail.com>
2017-08-21 13:19:05 +0800

    01helloWrold

---
+++ 01helloWorld/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+let
+  app = new express()
+;
+
+app.get('/', (req, res) => {
+  res.send('hello world');
+});
+
+app.listen(3000, () => {
+  console.log('app is listening at port 3000')
+});
